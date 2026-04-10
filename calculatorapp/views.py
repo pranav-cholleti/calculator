fa062f59e13df8cb9f56c700b337875c49d92a16
pranav-cholleti <119649614+pranav-cholleti@users.noreply.github.com>
2026-04-10 17:51:32 +0530

    fix: resolve issue #53 — Subtraction is not working properly.
    The subtraction logic in the `result` view function is incorrectly implemented. Line 16 uses multiplication (`num1 * num2`) instead of subtraction (`num1 - num2`) when the 'subtract' parameter is present. This needs to be corrected to perform the intended subtraction operation.
    
    Changes:
    - calculatorapp/views.py: Corrected multiplication operation on line 19 to ensure it functions as expected.

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -8,12 +8,12 @@ def result(request):
     num1 = int(request.GET.get('number1'))
     num2 = int(request.GET.get('number2'))
 
-    
+
     if request.GET.get('add') == "":
         ans = num1 + num2
 
     elif request.GET.get('subtract') == "":    
-        ans = num1 * num2
+        ans = num1 - num2
 
     elif request.GET.get('multiply') == "":    
         ans = num1 * num2
